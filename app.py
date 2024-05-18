9a6f87a48b80998632ef8a32c4af7d19ecb1d4da
Subhash Prasad <subhash.j.prasad@gmail.com>
2024-05-18 16:15:35 -0700

    Implemented UI and several room coordinates

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+from flask import Flask, jsonify, render_template, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
 
@@ -19,13 +19,18 @@ except Exception as e:
 
 db = client['building_database']
 
+app = Flask(__name__)
+
 day_to_number = {"M": 1, "TU": 2, "W": 3, "TH": 4, "F": 5, "SA": 6, "SU": 7}
 
-def get_available_rooms(day, time, duration):
+@app.route('/api/available_rooms', methods=['GET'])
+def get_available_rooms():
+    day = request.args.get('day')
+    time = float(request.args.get('time'))
+    duration = float(request.args.get('duration'))
     day_number = day_to_number[day]
 
     end_time = time + duration
-
     available_rooms_by_building = {}
 
     for building in db.list_collection_names():
@@ -42,15 +47,27 @@ def get_available_rooms(day, time, duration):
             }
         }
         available_rooms = collection.find(query, {"room_number": 1})
-
         available_room_numbers = [room["room_number"] for room in available_rooms]
+        available_rooms_by_building[building] = len(available_room_numbers)
 
-        available_rooms_by_building[building] = available_room_numbers
+    return jsonify(available_rooms_by_building)
 
-    return available_rooms_by_building
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-day = "W"
-time = 10.0
-duration = 3.0
-available_rooms = get_available_rooms(day, time, duration)
-print(available_rooms)
+@app.route('/api/token')
+def get_mapbox_token():
+    access_token = os.getenv('MAPBOX_ACCESS_TOKEN')
+    return jsonify({'accessToken': access_token})
+
+@app.route('/api/available_rooms')
+def available_rooms():
+    day = request.args.get('day')
+    time = float(request.args.get('time'))
+    duration = float(request.args.get('duration'))
+    rooms = get_available_rooms(day, time, duration)
+    return jsonify(rooms)
+
+if __name__ == '__main__':
+    app.run(debug=True)
